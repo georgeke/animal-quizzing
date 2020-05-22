3795203b996e4dde4118f48d4761bbeb66ac3033
George Ke <gkeee@instagram.com>
2020-05-22 18:43:11 -0400

    black formatting

--- server/models.py
+++ server/models.py
@@ -2,13 +2,16 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import List
 
+
 class TraitType(Enum):
     PERSONALITY = "personality"
+
 
 @dataclass
 class Answer:
     text: str
-    trait_value: str # TODO: change to enum?
+    trait_value: str  # TODO: change to enum?
+
 
 @dataclass
 class QuestionType:
