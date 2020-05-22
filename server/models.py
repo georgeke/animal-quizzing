f18cad10c62155affdaeb3270806c75c01904e7c
George Ke <gkeee@instagram.com>
2020-05-22 18:39:52 -0400

    import enum

--- server/models.py
+++ server/models.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import List
 
 class TraitType(Enum):
     PERSONALITY = "personality"
+
+@dataclass
+class Answer:
+    text: str
+    trait_value: str # TODO: change to enum?
 
 @dataclass
 class QuestionType:
@@ -10,8 +16,3 @@ class QuestionType:
     trait_type: TraitType
     score: int
     answers: List[Answer]
-
-@dataclass
-class Answer:
-    text: str
-    trait_value: str # TODO: change to enum?
