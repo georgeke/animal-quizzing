77c0cde3aa7f55c1322d2fd1e3354940f3c226ab
katrinacrisostomo <katrinacrisostomo@Katrinas-MacBook-Pro.local>
2020-05-23 13:21:34 -0400

    Add random generation of filter text questions

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 import random
 
 from flask import Flask, Response, request
-from loader import load_question_blueprints
-from models import Question
-from typing import Any, Dict
+from loader import load_question_blueprints, load_villagers
+from models import Question, Villager, QuestionBlueprint
+from typing import Any, Dict, Sequence
 
 app = Flask(__name__)
 
@@ -11,15 +11,10 @@ app = Flask(__name__)
 @app.route("/question", methods=["POST", "GET"])
 def question() -> Dict[str, Any]:
     user_answers_data = request.get_json()
+
     questions = load_question_blueprints()
-    random.shuffle(questions[0]["answers"])
-    question = Question(
-        questionId=questions[0]["questionId"],
-        questionText=questions[0]["questionText"],
-        questionFormat=questions[0]["questionFormat"],
-        villagerTrait=questions[0]["villagerTrait"],
-        answers=questions[0]["answers"][:4],
-    )
+    question = _generate_filter_question(questions)
+
     return {
         "answers": user_answers_data["answers"],
         "nextQuestion": question,
@@ -32,3 +27,34 @@ def update_cors(response: Response):
     response.headers.add("Access-Control-Allow-Methods", "GET,POST")
     response.headers.add("Access-Control-Allow-Credentials", "true")
     return response
+
+
+def _generate_filter_question(
+    questions: Sequence[QuestionBlueprint]
+) -> Question:
+    filter_question_ids = ["1", "2", "3", "4", "5"]
+    # eventually we will also add "10", "11", "12", "13", "14", "15", "16", "17"
+
+    random.shuffle(filter_question_ids)
+    question_id = filter_question_ids[0]
+
+    question_blueprint = _get_question_with_id(questions, question_id)
+
+    random.shuffle(question_blueprint["answers"])
+
+    return Question(
+        questionId=question_blueprint["questionId"],
+        questionText=question_blueprint["questionText"],
+        questionFormat=question_blueprint["questionFormat"],
+        villagerTrait=question_blueprint["villagerTrait"],
+        answers=question_blueprint["answers"][:4],
+    )
+
+
+def _get_question_with_id(
+    questions: Sequence[QuestionBlueprint],
+    id: str
+) -> QuestionBlueprint:
+    for question in questions:
+        if question["questionId"] == id:
+            return question
