bb6ab91dd90415ed582d5ed87c648f9fdc1c4b0f
George Ke <gkeee@instagram.com>
2020-05-23 00:28:50 -0400

    CORS

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, make_response, request
 from loader import load_question_blueprints
 from models import Question
 from typing import Any, Dict
@@ -10,10 +10,13 @@ app = Flask(__name__)
 def question() -> Dict[str, Any]:
     user_answers_data = request.get_json()
     questions = load_question_blueprints()
-    return Question(
+    question = Question(
         questionId=questions[0]["questionId"],
         questionText=questions[0]["questionText"],
         questionFormat=questions[0]["questionFormat"],
         villagerTrait=questions[0]["villagerTrait"],
         answers=questions[0]["answers"][:4],
     )
+    response = make_response(question)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
