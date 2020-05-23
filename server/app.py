1c287f8061a5d3511e23c94151c5ce908411bc07
George Ke <gkeee@instagram.com>
2020-05-22 20:54:52 -0400

    basic question endpoint

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from loader import load_questions
+from typing import Any, Dict
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Animal Quizzing!"
+@app.route("/question", methods=["POST", "GET"])
+def question() -> Dict[str, Any]:
+    user_answers_data = request.get_json()
+    questions = load_questions()
+    questions[0]["answers"] = questions[0]["answers"][:4]
+    return questions[0]
